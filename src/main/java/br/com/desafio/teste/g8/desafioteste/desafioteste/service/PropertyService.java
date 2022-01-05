04b600770681ada856f869dc7479daa239072a11
vfeitoza <vinicius.feitoza@mercadolivre.com>
2022-01-05 17:03:21 -0300

    adiciona - uso do repository nos metodos

--- src/main/java/br/com/desafio/teste/g8/desafioteste/desafioteste/service/PropertyService.java
+++ src/main/java/br/com/desafio/teste/g8/desafioteste/desafioteste/service/PropertyService.java
@@ -2,21 +2,26 @@ package br.com.desafio.teste.g8.desafioteste.desafioteste.service;
 
 import br.com.desafio.teste.g8.desafioteste.desafioteste.entity.Property;
 import br.com.desafio.teste.g8.desafioteste.desafioteste.entity.Room;
+import br.com.desafio.teste.g8.desafioteste.desafioteste.repository.PropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
 @Service
 public class PropertyService {
+    @Autowired
+    private PropertyRepository propertyRepository;
     /**
      * @author Vinicius Feitoza
      * @description Método que determina a área de cada comodo.
-     * @param property
+     * @param name
      * @return Retorna um Hashmap de comodos com suas áreas.
      */
-    public HashMap<String, Double> getRoomArea(Property property) {
+    public HashMap<String, Double> getRoomArea(String name) {
+        Property p = this.propertyRepository.findByName(name);
         HashMap<String, Double> roomArea = new HashMap<String, Double>();
-        for (Room room: property.getQuartoList()){
+        for (Room room: p.getQuartoList()){
             double area = room.getWidth() * room.getLength();
             roomArea.put(room.getName(), area);
         }
@@ -26,11 +31,12 @@ public class PropertyService {
     /**
      * @author Vinicius Feitoza
      * @description Método para calcular a área total do imóvel com base na soma dos comodos.
-     * @param property
+     * @param name
      * @return Retorna um double da área do imovel.
      */
-    public double getPropertyArea(Property property) {
-        HashMap<String, Double> roomArea = this.getRoomArea(property);
+    public double getPropertyArea(String name) {
+        Property p = this.propertyRepository.findByName(name);
+        HashMap<String, Double> roomArea = this.getRoomArea(p.toString());
         double propertyArea = roomArea.values().stream().reduce((a, b) -> a + b).get();
         return propertyArea ;
     }
