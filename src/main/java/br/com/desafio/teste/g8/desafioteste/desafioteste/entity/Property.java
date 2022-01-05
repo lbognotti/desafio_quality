7c8246ac72c7c478c61bb4af0509deedb3be04eb
vfeitoza <vinicius.feitoza@mercadolivre.com>
2022-01-05 12:57:32 -0300

    adiciona - metodo getRoomArea

--- src/main/java/br/com/desafio/teste/g8/desafioteste/desafioteste/entity/Property.java
+++ src/main/java/br/com/desafio/teste/g8/desafioteste/desafioteste/entity/Property.java
@@ -3,9 +3,12 @@ package br.com.desafio.teste.g8.desafioteste.desafioteste.entity;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Builder
 public class Property {
     private String name;
     private String district;
+    private List<Room> quartoList;
 }
--- src/main/java/br/com/desafio/teste/g8/desafioteste/desafioteste/service/PropertyService.java
+++ src/main/java/br/com/desafio/teste/g8/desafioteste/desafioteste/service/PropertyService.java
@@ -1,7 +1,25 @@
 package br.com.desafio.teste.g8.desafioteste.desafioteste.service;
 
+import br.com.desafio.teste.g8.desafioteste.desafioteste.entity.Property;
+import br.com.desafio.teste.g8.desafioteste.desafioteste.entity.Room;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 public class PropertyService {
+    /**
+     * @author Vinicius Feitoza
+     * @description Método que determina a área de cada comodo.
+     * @param property
+     * @return Retorna um Hashmap de comodos com suas áreas.
+     */
+    public HashMap<String, Double> getRoomArea(Property property) {
+        HashMap<String, Double> roomArea = new HashMap<String, Double>();
+        for (Room room: property.getQuartoList()){
+            double area = room.getWidth() * room.getLength();
+            roomArea.put(room.getName(), area);
+        }
+        return roomArea;
+    }
 }
