4dd368550874653d186c44da21d733365d7f69e2
lbognotti <lucas.bognotti@mercadolivre.com>
2022-01-05 09:49:51 -0300

    first commit

---
+++ src/main/java/br/com/desafio/teste/g8/desafioteste/desafioteste/DesafiotesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.teste.g8.desafioteste.desafioteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiotesteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafiotesteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/desafio/teste/g8/desafioteste/desafioteste/DesafiotesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.teste.g8.desafioteste.desafioteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiotesteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
